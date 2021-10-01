3815534cdbfd9f89390b7afdaebb47d8f8a5f64b
Trentp26 <trentpoitras@gmail.com>
2021-10-01 15:30:56 -0400

    added user input and cheater code to stop game

--- index.js
+++ index.js
@@ -9,21 +9,32 @@ function ask(questionText) {
 
 start();
 
-let max = 100;
-let min = 1;
-
-let compNum = 50;
 async function start() {
+  let max = 100;
+  let min = 1;
+
+  let compNum = 50;
   console.log(
     "Let's play a game where you (human) make up a number and I (computer) try to guess it."
   );
-  //max = await ask(`what would you like your maximum guess to be?`);
-  //min = await ask(`what you you like your minimum guess to be?`);
   //compNum = (max + min) / 2;
+  //letting the player set new max in min
+  let newMax = await ask(`what would you like your maximum guess to be?`);
+  max = parseInt(newMax);
+  console.log(max);
+
+  let newMin = await ask(`What would you like you minimum guess to be`);
+  min = parseInt(newMin);
+  console.log(min);
+
+  //setting compNum to the median of the range
+  compNum = Math.floor((max + min) / 2);
+  console.log(compNum);
 
   let secretNumber = await ask(
     "What is your secret number?\nI won't peek, I promise...\n"
   );
+
   console.log("You entered: " + secretNumber);
   // Now try and complete the program.
   // start with 50(compNum) and ask if it is there number
@@ -34,9 +45,67 @@ async function start() {
 
     //process.exit();
   }
+  function gameStart(yesOrNo) {
+    //run a loop to see if answer is yes or no
+    if (yesOrNo === "yes") {
+      //if yes tell them you found number and end
+      console.log(`i have found you number... ${compNum}!!!`);
+      process.exit();
+      //if yesOrNo is no run next function High or low
+    } else if (yesOrNo === `no`) {
+      isItHighOrLow();
+    }
+  }
+  async function isItHighOrLow() {
+    // ask if it id higher or lower and store as a value
+    while (true) {
+      let highOrLow = await ask(`is this higher(high) or lower(low)`);
+      if (highOrLow === "high") {
+        //if answer is higher run is high function
+        await isHigh();
+      } else if (highOrLow === `low`) {
+        //if answer is lower run isLower function
+        await isLow();
+      }
+    }
+  }
+  async function isHigh() {
+    // getting rid of off by one
+    min = compNum + 1;
+    console.log(min);
+    console.log(max);
+    //finding the median as a new guess
+    compNum = Math.floor((min + max) / 2);
+    if (min > max || max < min) {
+      console.log(`Stop it you are lying!!!`);
+      process.exit();
+    }
+    yesOrNo = await ask(`My guess is... ${compNum} is this your number???`);
+    if (yesOrNo === `yes`) {
+      process.exit();
+    }
+  }
+  async function isLow() {
+    //getting rid of off by one
+    //setting max to new number
+    max = compNum - 1;
+    console.log(min);
+    console.log(max);
+    //finding the median as a new guess
+    compNum = Math.floor((min + max) / 2);
+
+    if (min > max || max < min) {
+      console.log(`Stop it you are lying!!!`);
+      process.exit();
+    }
+    yesOrNo = await ask(`My guess is... ${compNum} is this your number?`);
+    if (yesOrNo === `yes`) {
+      proc;
+    }
+  }
 }
 
-async function gameStart(yesOrNo) {
+/*async function gameStart(yesOrNo) {
   //run a loop to see if answer is yes or no
   while (true) {
     if (yesOrNo === "yes") {
@@ -88,4 +157,4 @@ async function isLow() {
   if (yesOrNo === `yes`) {
     process.exit();
   }
-}
+}*/
